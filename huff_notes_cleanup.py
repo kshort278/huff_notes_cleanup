5b6189e168ed7f2a5a19d0934c7e1a346b786226
Kathy Short <kshort278@gmail.com>
2020-04-21 10:51:47 -0400

    clean up huff_notes_cleanup.py script
    remove extra comments

--- huff_notes_cleanup.py
+++ huff_notes_cleanup.py
@@ -2,24 +2,22 @@
 
 
 def main():
-    #define old file and new file to write to
+
     source_file = "huff_program_notes_1950s.txt"
     target_file = "huff_program_notes_1950s_goodData.txt"
 
-    #open both files and read source, write to target
     s = open(source_file, 'r')
     t = open(target_file, 'w')
     
     count = 0
 
-    #for each line of data in the file
     for line in s:
 
-        #take off "new line" and add a * as a marker for the end of each line
+        #take off new line character and add a * as a marker for the end of each line
         text_line = line.rstrip('\n')
         text_line = text_line + '*'
 
-        #to get rid of "Program Date" column in header
+        #get rid of "Program Date" column in header
         #then strip * and add new line then write to new file
         if (text_line[:12] == "Program Date" ):
             new_text = text_line[13:]
@@ -72,14 +70,11 @@ def main():
             t.write(new_text)
             count +=1
 
-    #each record formatted besides header is written to new file
-    #then counted using "count+=1"
+    #each record formatted except header is written to new file
 
-    #close both files
     s.close()
-    t.close
+    t.close()
 
-    #print how many records were written to the new file
     print("File written. %d records. " % count)
 
 main()
